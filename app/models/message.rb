14f3f04cbd6e81a77fffc552372c2b799b5fccf4
Artem <artyezersky@gmail.com>
2019-12-12 07:30:24 +0200

    new rspec tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   include Hashid::Rails
   validates :message, presence: true
+  validates_inclusion_of :delaydelete, in: [true, false]
   #secret_key = ENV['DB_COL_ENCRYPTED_KEY']
   super_key = SecureRandom.hex(16)
   attr_encrypted :message, :key => super_key
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-
-
-    it "should return encrypted message" do
-      message = Message.new(message: 'Some new message', delaydelete: true)
-      message.save
-      puts message.message
+    it "create message" do
+      message = Message.create(message: 'Some new message', delaydelete: true)
+      message1 = Message.create(message: 'Some new message1', delaydelete: false)
+      expect(message.message).to eq('Some new message')
+      expect(message.delaydelete).to eq(true)
+      expect(message1.message).to eq('Some new message1')
+      expect(message1.delaydelete).to eq(false)
     end
 
+    it "should return true if link is encrypted" do
+      message = Message.new(message: 'Some new message', delaydelete: true)
+      message.save
+      expect(message_path(message.id)).not_to eq(message_path(message))
+      expect(message_path(message.id).length).to be < message_path(message).length
+    end
+
+    it "shouldn't save invalid data" do
+      message = Message.new(message: 'Some new message', delaydelete: nil)
+      message1 = Message.new(message: '', delaydelete: true)
+      expect(message.save).to eq(false)
+      expect(message1.save).to eq(false)
+    end
 
 end
